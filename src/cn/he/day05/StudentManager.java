7ae5fdebb254b0a3233d8bc3a79d3a41ff3f5f71
xiaohe <3168738793@qq.com>
2023-01-01 22:02:24 +0800

    java学生信息管理系统

--- src/cn/he/day05/StudentManager.java
+++ src/cn/he/day05/StudentManager.java
@@ -1,27 +1,30 @@
 package cn.he.day05;
 
 import cn.he.JdbcTool.JdbcUtil;
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 /*
     学生信息管理系统
  */
 public class StudentManager {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         while(true){
-            Scanner input = new Scanner(System.in);
             System.out.print("a:添加学生信息\t");
-            System.out.print("b:查询所有学生信息\t");
+            System.out.print("b:查询学生信息\t");
             System.out.print("c:根据id修改学生信息\t");
             System.out.print("d:根据id删除学生信息\t");
             System.out.println("e:退出系统\t");
             System.out.println("请输入操作，abcde任选一项：");
-            String a = input.nextLine();
+            String a = sc.next();
             switch(a){
                 case "a":addStu(); break;
-                case "b":findAll(); break;
+                case "b":selectStu(); break;
                 case "c":updateStu(); break;
                 case "d":deleteStu(); break;
                 case "e":System.out.println("e.退出成功！");return;
@@ -29,40 +32,92 @@ public class StudentManager {
             }
         }
     }
-    public static void findAll(){//查询信息
+
+    public static void selectStu(){//查询信息
         System.out.println("b.查询学生信息------>");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入学号和姓名：");
-        String stuid = sc.nextLine();
-        String stuname = sc.nextLine();
+        System.out.println("a.查询所有学生信息\tb.查询单个学生信息\tc.退出");
+        String in = sc.next();
 
         //要连接的数据库名字
         String database = "jt_db";
         Connection con = null;
         PreparedStatement ps = null;
+        ResultSet rs = null;
+        if ("a".equals(in)) {
+            try {
+                con = JdbcUtil.getCon(database);
+                String sql  = "select * from stu";
+                ps = con.prepareStatement(sql);
+                rs = ps.executeQuery();
+                while(rs.next()){
+                    String stuid = rs.getString("stuid");
+                    String name = rs.getString("name");
+                    String gender = rs.getString("gender");
+                    String addr = rs.getString("addr");
+                    double score = rs.getDouble("score");
+                    System.out.println(stuid + ", " + name + ", " + gender + ", " + addr + ", " + score);
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } finally {
+                JdbcUtil.close(con, ps, rs);
+            }
+        } else if ("b".equals(in)) {
+            findStu();
+        } else if ("c".equals(in)) {
+            return;
+        } else {
+            System.out.println("你的输入有误，请重试！");
+        }
+    }
+
+    public static void findStu(){
+        System.out.println("请输入学号：");
+        String stuid = sc.next();
+
+        //要连接的数据库名字
+        String database = "jt_db";
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         try {
             con = JdbcUtil.getCon(database);
-            String sql  = "";
+            String sql  = "select * from stu where stuid = ? ;";
             ps = con.prepareStatement(sql);
-            System.out.println("加载驱动，连接"+con+"成功！");
+            ps.setString(1,stuid);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                String name = rs.getString("name");
+                String gender = rs.getString("gender");
+                String addr = rs.getString("addr");
+                double score  = rs.getDouble("score");
+                System.out.println("学号："+stuid);
+                System.out.println("姓名："+name);
+                System.out.println("性别："+gender);
+                System.out.println("地址："+addr);
+                System.out.println("分数："+score);
+            } else{
+                System.out.println("学号"+stuid+"学生不存在！");
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            JdbcUtil.close(con, ps, rs);
         }
     }
     
     public static void addStu(){
         System.out.println("a.添加学生信息------->");
-        Scanner input = new Scanner(System.in);
         System.out.println("请输入学号：");
-        String stuid = input.next();
+        String stuid = sc.next();
         System.out.println("请输入名字：");
-        String stuname = input.next();
+        String stuname = sc.next();
         System.out.println("请输入性别：");
-        String sex = input.next();
+        String sex = sc.next();
         System.out.println("请输入住址：");
-        String stuaddr = input.next();
+        String stuaddr = sc.next();
         System.out.println("请输入分数：");
-        double score = input.nextDouble();
+        double score = sc.nextDouble();
 
         //要连接的数据库名字
         String database = "jt_db";
@@ -70,7 +125,6 @@ public class StudentManager {
         PreparedStatement ps = null;
         try {
             con = JdbcUtil.getCon(database);
-            System.out.println("加载驱动，连接"+con+"成功！");
             String sql = "insert into stu(stuid, name, gender, addr, score)" +
                     "values(?, ?, ?, ?, ?)";
             ps = con.prepareStatement(sql); //SQL骨架就已经确定了
@@ -81,7 +135,7 @@ public class StudentManager {
             ps.setDouble(5,score);
             int rows = ps.executeUpdate();
             if(rows>0){
-                System.out.println("学生:"+stuname+"添加成功！");
+                System.out.println("学生 "+stuname+" 添加成功！");
             }
             else{
                 System.out.println("添加失败，请联系管理员！");
@@ -96,12 +150,48 @@ public class StudentManager {
 
     public static void updateStu() {//修改学生信息
         System.out.println("c.修改学生信息------>");
+        findStu();
+        System.out.println("请输入要修改的学号、姓名、性别、地址、分数");
+        String id = sc.next();
+        String name = sc.next();
+        String gender = sc.next();
+        String addr = sc.next();
+        double score = sc.nextDouble();
 
+        //要连接的数据库名字
+        String database = "jt_db";
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
     }
 
     public static void deleteStu() {//删除学生信息
         System.out.println("d.删除学生信息------>");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要删除的学生学号");
+        String stuid = sc.next();
 
+        //要连接的数据库名字
+        String database = "jt_db";
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = JdbcUtil.getCon(database);
+            String sql = "delete from stu where stuid = ?;";
+            ps = con.prepareStatement(sql); //SQL骨架就已经确定了
+            ps.setString(1,stuid);
+            int rows = ps.executeUpdate();
+            if (rows>0){
+                System.out.println("id："+stuid+"信息删除成功！");
+            }
+            else{
+                System.out.println("删除失败，请重试！");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            JdbcUtil.close(con);
+            JdbcUtil.close(ps);
+        }
     }
-
 }
